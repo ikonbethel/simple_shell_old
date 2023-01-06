56bdab6edbb668909114606c095bdf42d721d551
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2023-01-06 13:43:14 +0100

    Create executeB.c

---
+++ executeB.c
@@ -0,0 +1,22 @@
+#include "header.h"
+
+/**
+ * is_path - checks if input command is part of directory PATH
+ * @command: a command
+ *
+ * Return: 1 if path, 0 if no path
+ */
+int is_path(char *command)
+{
+	int i;
+
+	i = 0;
+	while (command[i] != '\0')
+	{
+		if (command[i] == '/')
+			return (1);
+		i++;
+	}
+
+		return (0);
+}
